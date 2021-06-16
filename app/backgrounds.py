b20c0d69ecf63bccee05d92ba8a39fffe0f49334
efrabaez <e.german@hotmail.com>
2021-06-16 13:19:18 -0500

    Update to get only 1 bg

--- app/backgrounds.py
+++ app/backgrounds.py
@@ -1,12 +1,12 @@
-
 from random import randint, random
 
+scss_files = ['purple.css']
 
-scss_files = ['bubbles.css', 'purple.css']
 
 def get_random_background():
     random_index = randint(0, len(scss_files) - 1)
-    #return "{{ url_for('static',filename='styles/backgrounds/{" + scss_files[random_index] +  "}') }}"
+    # return "{{ url_for('static',filename='styles/backgrounds/{" + scss_files[random_index] +  "}') }}"
     return scss_files[random_index]
 
-print(get_random_background())
+
+
