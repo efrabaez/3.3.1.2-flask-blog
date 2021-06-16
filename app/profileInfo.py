a5e8f07e2ac1d3208c7ccc225fd98d9d24876487
efrabaez <e.german@hotmail.com>
2021-06-16 13:21:26 -0500

    Add support for utf-8 enconding

--- app/profileInfo.py
+++ app/profileInfo.py
@@ -1,10 +1,11 @@
 import json
 import os
 
-app_dir = os.path.dirname(__file__) #<-- app directory
+app_dir = os.path.dirname(__file__)  # <-- app directory
+
 
 def get_profile_data(filename):
     filename = os.path.join(app_dir, filename)
-    with open(filename) as file:
+    with open(filename, encoding='utf-8') as file:
         data = json.load(file)
         return data
